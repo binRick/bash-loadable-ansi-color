a0687dbab93a0dd9651671ed26fb1e517eca285c
root <root@f200.vpnservice.company>
2021-11-24 15:23:18 -0500

    m

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ set -e
 td=$(mktemp -d)
 of=$td/stdout
 QTY=119
+if ! command -v ansi; then
+  alias ansi=$(pwd)/ansi
+fi
 
 if ! ( (./bootstrap.sh && ./configure && make clean && make) 2>&1 | pv -s $QTY -l -N "Compiling") >$of; then
 	cat $of
