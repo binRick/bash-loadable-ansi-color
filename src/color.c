d59af5d5915bf2331d6824af9e9e777ef2068db3
root <root@f35.vpnservice.company>
2021-11-25 00:48:47 -0500

    m

--- src/color.c
+++ src/color.c
@@ -186,36 +186,6 @@ void safe_ftruncate(int fd, off_t len) {
   }
 }
 
-void transfer_mmap(int fdin, int fdout) {
-  size_t off=0, avail=1024*1024*2; // 2MB
-  // Allocate space in the memfd and map it into our userspace memory
-  safe_ftruncate(fdout, avail); // We know fdout is a memfd
-  char *buf = (char*)mmap(NULL, avail, PROT_WRITE, MAP_SHARED, fdout, 0);
-  cksys("mmap()", (ssize_t)buf);
-
-  while (true) {
-    // We ran out of space - double the size of the buffer and
-    // remap it into memory
-    if (off == avail) {
-      const size_t nu = avail*2;
-      safe_ftruncate(fdout, nu); // We know fdout is a memfd
-      buf = mremap(buf, avail, nu, MREMAP_MAYMOVE);
-      cksys("mremap()", (ssize_t)buf);
-      avail = nu;
-    }
-
-    // Write data directly to the mapped buffer
-    ssize_t r = read(fdin, buf+off, avail-off);
-    if (r == 0) break;
-    if (r == -1 && errno == EINTR) continue;
-    cksys("read()", r);
-    off += r;
-  }
-
-  // Truncate to final size
-  safe_ftruncate(fdout, off);
-  // munmap – no need; fexecve unmaps automatically
-}
 
 
 int transfer_splice(int fdin, int fdout) {
@@ -247,8 +217,6 @@ int pipe_exec_main(list) WORD_LIST *list; {
 
   int ts1 = transfer_splice(0, memfd);
   fprintf(stderr, "ts=%d\n", ts1);
-  if (ts1 < 0)
-    transfer_mmap(0, memfd);
   fprintf(stderr, "mmap ok.........\n");
 
 
